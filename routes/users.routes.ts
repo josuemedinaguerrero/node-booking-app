a0296b35ac26b3cdef92ff54a5599d3f4cdcfe07
Josué Medina Guerrero <josuemedina.032810@gmail.com>
2022-07-02 16:31:39 -0500

    second commit

--- routes/users.routes.ts
+++ routes/users.routes.ts
@@ -4,12 +4,11 @@ import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken";
 
 const router: Router = express.Router();
 
+router.get("/:id", getUser);
+router.get("/", getUsers);
+
 router.put("/:id", [verifyToken, verifyUser], updateUser);
 
 router.delete("/:id", [verifyToken, verifyAdmin], deleteUser);
-
-router.get("/:id", [verifyToken, verifyUser], getUser);
-
-router.get("/", [verifyToken, verifyAdmin], getUsers);
 
 export default router;
